557ecdc1b5a6b150f42f61b6245dd4ff4244a045
ABHILASH <abhi.badamoni@gmail.com>
2026-09-22 11:06:20 -0400

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE='civilized-men-v3';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/assets/icons/icon.svg','/assets/css/main.css','/assets/js/products.js','/assets/js/app.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
+
